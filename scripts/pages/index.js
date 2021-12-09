6076dbc628a8a6fa5f51a86739e0225502c46938
Thomas Dejardins <tdejardins@gmail.com>
2021-12-09 12:59:12 +0100

    feat(index.js): recover data from json file #1

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,12 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // les données récupérées dans le fichier json
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+            const data = JSON.parse(this.responseText);
+            displayData(data.photographers);
+        }
+        xmlhttp.open("GET", "/data/photographers.json");
+        xmlhttp.send();
     }
 
     async function displayData(photographers) {
@@ -37,9 +21,9 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        const photographers = await getPhotographers();
     };
     
     init();
+    
     
