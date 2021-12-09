394f1d52aa68d2d709e6ae8fdbad4d75e4d4ca0f
Thomas Dejardins <tdejardins@gmail.com>
2021-12-09 13:05:23 +0100

    feat(factories/photographer.js): add elements to display data #1

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const divPresentation = document.createElement('div');
+        divPresentation.className = "presentation";
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const divInformation = document.createElement('div');
+        divInformation.className = "informations";
+        const pCity = document.createElement('p');
+        pCity.className = "location";
+        pCity.textContent = city + ", " + country;
+        const pTagline = document.createElement('p');
+        pTagline.className = "tagline";
+        pTagline.textContent = tagline;
+        const pPrice = document.createElement('p');
+        pPrice.className = "price"
+        pPrice.textContent = price + "€/jour";
+
+        article.appendChild(divPresentation);
+        divPresentation.appendChild(img);
+        divPresentation.appendChild(h2);
+        article.appendChild(divInformation);
+        divInformation.appendChild(pCity);
+        divInformation.appendChild(pTagline);
+        divInformation.appendChild(pPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
